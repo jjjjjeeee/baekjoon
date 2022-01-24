9c82113e1476daad945df6654206d7d3c58e5224
jeong eun Lee <monami6901@gmail.com>
2022-01-24 20:37:45 +0900

    4th commit

--- src/Study/S_11866.java
+++ src/Study/S_11866.java
@@ -7,30 +7,30 @@ import java.util.Queue;
 public class S_11866 {
 	// 요세푸스 문제
 
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			int n= scan.nextInt();
-			int k = scan.nextInt();
-			Queue<Integer> q = new LinkedList<>(); 
-			
-			for(int i=0;i<n;i++) {
-				q.add(i+1);
-			}
-			
-			System.out.print("<");
-			while (q.size()>0){
-	        	// 맨 앞에있던 숫자를 poll한 후 큐에 새로 추가하여 큐의 시작을 변경
-				for(int i=0;i<k-1;i++) {
-					q.add(q.poll());
-				}
-				System.out.print(q.poll());
-				if (q.size()>0) {
-					System.out.print(", ");
-				}
-			}
-			System.out.print(">");
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n= scan.nextInt();
+		int k = scan.nextInt();
+		Queue<Integer> q = new LinkedList<>(); 
+
+		for(int i=0;i<n;i++) {
+			q.add(i+1);
 		}
 
+		System.out.print("<");
+		while (q.size()>0){
+			// 맨 앞에있던 숫자를 poll한 후 큐에 새로 추가하여 큐의 시작을 변경
+			for(int i=0;i<k-1;i++) {
+				q.add(q.poll());
+			}
+			System.out.print(q.poll());
+			if (q.size()>0) {
+				System.out.print(", ");
+			}
+		}
+		System.out.print(">");
 	}
+
+}
 
 
--- src/Study/S_11866_2.java
+++ src/Study/S_11866_2.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class S_11866_2 {
-
+	// 요세푸스 문제
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
@@ -13,7 +13,7 @@ public class S_11866_2 {
 		int k = s.nextInt();
 		
 		Queue<Integer> q = new LinkedList<Integer>();
-		StringBuilder res = new StringBuilder();
+		StringBuilder res = new StringBuilder(); // 기존의 데이터에 더하는 방식
 		
 		for(int i=1; i<=n; i++) {
 			q.offer(i);
@@ -25,7 +25,7 @@ public class S_11866_2 {
 			for(int m=0; m<k-1; m++) {
 				q.offer(q.poll());
 			}
-			res.append(q.poll()+",");
+			res.append(q.poll()+","); // res에 poll로 추출된 번호 추가
 		}
 		res.append(q.poll()+">");
 		System.out.println(res);
